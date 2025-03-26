2096a41f01291a09e3a9da4c8f0c8d510b3c68a2
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-03-26 17:39:21 -0300

    Implementada validação de entrada

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -12,9 +12,24 @@
             Console.WriteLine("Para começar digite o tamanho central do diamante!");
             Console.Write("Tamanho: ");
             tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(tamanhoDiamante);
+
+            while(!ValidarEntrada(tamanhoDiamante))
+            {
+                Console.WriteLine("O tamanho do diamante deve ser um número ímpar!");
+                Console.Write("Tamanho: ");
+                tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+            }
 
             Console.ReadLine();
+        }
+
+        static bool ValidarEntrada(int tamanhoDiamante)
+        {
+            if (tamanhoDiamante % 2 == 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
